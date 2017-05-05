fab8b550b7f985a260fed16e8427207321dffec1
Steevan BARBOYON <steevan.barboyon@gmail.com>
2017-05-05 17:00:05 +0200

    Start ConsoleBundle

---
+++ Application.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Huttopia\ConsoleBundle;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application as BundleApplication;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class Application extends BundleApplication
+{
+    protected $excludedCommands = [];
+
+    public function addExcludedCommand(string $name): self
+    {
+        $this->excludedCommands[] = $name;
+
+        return $this;
+    }
+
+    public function getExlucdedCommands(): array
+    {
+        $return = $this->excludedCommands;
+
+        // help and list commands are added before container instantiation
+        if ($this->getKernel()->getContainer() instanceof ContainerInterface) {
+            $return = array_merge($return, $this->getKernel()->getContainer()->getParameter('console.excluded'));
+        }
+
+        return $return;
+    }
+
+    public function add(Command $command)
+    {
+        if (
+            count(
+                array_intersect(
+                    array_merge([$command->getName()], $command->getAliases()),
+                    static::getExlucdedCommands()
+                )
+            ) === 0
+        ) {
+            $return = parent::add($command);
+        } else {
+            $return = null;
+        }
+
+        return $return;
+    }
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Huttopia\ConsoleBundle\DependencyInjection;
+
+use Symfony\Component\Config\{
+    Definition\Builder\TreeBuilder,
+    Definition\ConfigurationInterface
+};
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('console');
+
+        $rootNode
+            ->children()
+                ->arrayNode('excluded')
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
